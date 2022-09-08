f2b7be875c6a731d95ed0dbbc48aa019e53f081a
Ruddhi Prasad Panda <45842528+rpPanda@users.noreply.github.com>
2022-09-08 11:12:01 +0530

    Use library to get default cred (#15)
    https://github.com/workduck-io/mex-loch/pull/15

--- src/libs/tokenHandler.ts
+++ src/libs/tokenHandler.ts
@@ -1,47 +1,23 @@
-import got from 'got'
-
-export interface PublicAccessCreds {
-  idToken?: string
-  accessToken?: string
-  refreshToken: string
-  expiry?: number
-}
+import { DefaultAccessCreds } from '@workduck-io/mex-default-user-token'
 
 const REFRESH_TOKEN = process.env.MEX_SERVICE_REFRESH_TOKEN
 const CLIENT_ID = process.env.MEX_SERVICE_CLIENT_ID
 
-export const initialPublicCreds = (): PublicAccessCreds => {
-  return { refreshToken: REFRESH_TOKEN }
+const initialPublicCreds = () => {
+  //expiry set to zero to force refreshToken fort he first time
+  return { refreshToken: REFRESH_TOKEN, expiry: 0 }
 }
+let accessCreds = initialPublicCreds()
 
-export const refreshAccessCreds = async (refreshToken: string): Promise<PublicAccessCreds> => {
-  const URL = 'https://workduck.auth.us-east-1.amazoncognito.com/oauth2/token'
-
-  const response = (await got
-    .post(URL, {
-      form: {
-        grant_type: 'refresh_token',
-        client_id: CLIENT_ID,
-        refresh_token: REFRESH_TOKEN
-      }
-    })
-    .json()) as any
-
-  return {
-    idToken: `Bearer ${response.id_token}`,
-    accessToken: `Bearer ${response.access_token}`,
-    refreshToken: refreshToken,
-    expiry: Date.now() + response.expires_in * 1000
+const defaultCreds = new DefaultAccessCreds(
+  REFRESH_TOKEN,
+  CLIENT_ID,
+  () => accessCreds,
+  (creds) => {
+    accessCreds = creds
   }
-}
-
-export let accessCreds: PublicAccessCreds = initialPublicCreds()
+)
 
 export const getCreds = async () => {
-  if (accessCreds.accessToken && Date.now() < accessCreds.expiry) {
-    return accessCreds
-  } else {
-    accessCreds = await refreshAccessCreds(accessCreds.refreshToken)
-    return accessCreds
-  }
+  return await defaultCreds.getCred()
 }
