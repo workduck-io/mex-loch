bbc363227949ab549402f17db4e976f5b1e1d47a
Vivek Raj <vivekraj3200@gmail.com>
2022-10-04 20:10:54 +0530

    rebase with main

--- handlers/whatsapp.ts
+++ handlers/whatsapp.ts
@@ -10,7 +10,7 @@ app.get('/whatsapp', (req, res) => {
    * UPDATE YOUR VERIFY TOKEN
    *This will be the Verify Token value when you set up webhook
    **/
-  const verify_token = 'qwertyasdfg'
+  const verify_token = process.env.WHATSAPP_VERIFY_TOKEN
 
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode']
@@ -33,7 +33,6 @@ app.get('/whatsapp', (req, res) => {
 const messageIds: string[] = []
 // Accepts POST requests at /webhook endpoint
 app.post('/whatsapp', async (req, res) => {
-
   // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
   if (req.body.object) {
     if (
@@ -43,38 +42,34 @@ app.post('/whatsapp', async (req, res) => {
       req.body.entry[0].changes[0].value.messages &&
       req.body.entry[0].changes[0].value.messages[0]
     ) {
-      let messageId = req.body.entry[0].changes[0].value.messages[0].id
-      let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id
-      let from = req.body.entry[0].changes[0].value.messages[0].from // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body // extract the message text from the webhook payload
+      let message = req.body.entry[0].changes[0].value
+      let from = message.messages[0].from // extract the phone number from the webhook payload
+      let msg_body = message.messages[0].text.body // extract the message text from the webhook payload
+      let phone_number_id = message.metadata.phone_number_id
 
-      const sendingUrl = `https://graph.facebook.com/v12.0/' + ${phone_number_id} + '/messages?access_token=' + ${process.env.WHATSAPP_BOT_TOKEN} `
+      const sendingUrl = `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${process.env.WHATSAPP_BOT_TOKEN}`
+      // console.log({ sendingUrl })
 
       try {
-        messageIds.push(messageId)
-        //check if the unique message id in messageIds array
-        const hasDuplicates = (arr) => arr.length !== new Set(arr).size
-        if (!hasDuplicates(messageIds)) {
-          const replyMessage = await messageService.handleMessage({
-            message: msg_body,
-            serviceId: from,
-            sourceUrl: `https://w.me/Mex-Offical-Bot/${req.body.entry[0].changes[0].value.messages[0].id}`,
-            serviceType: 'WHATSAPP'
-          })
-          if (replyMessage) {
-            const request: any = {}
-            request.json = {
-              messaging_product: 'whatsapp',
-              to: from,
-              text: { body: 'Ack: ' + msg_body }
-            }
-            request.headers = {
-              'Content-Type': 'application/json'
-            }
-            const response = await got.post(sendingUrl, request)
-            // console.log({ response })
-            res.sendStatus(200)
+        const replyMessage = await messageService.handleMessage({
+          message: msg_body,
+          serviceId: from,
+          sourceUrl: `https://w.me/Mex-Offical-Bot/${message.messages[0].id}}`,
+          serviceType: 'WHATSAPP'
+        })
+        if (replyMessage) {
+          const request: any = {}
+          request.json = {
+            messaging_product: 'whatsapp',
+            to: from,
+            text: { body: 'Ack: ' + msg_body }
           }
+          request.headers = {
+            'Content-Type': 'application/json'
+          }
+          const response = await got.post(sendingUrl, request)
+          // console.log({ response })
+          res.sendStatus(200)
         }
       } catch (error) {
         console.log('error', error)
